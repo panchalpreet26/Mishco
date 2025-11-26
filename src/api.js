ae4df6f438dfe2dfa5d2dceb453a45fdb1343ed4
Preet Panchal <panchalpreet26@gmail.com>
2025-11-26 11:42:12 +0530

    26-11-2025

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 // src/api.js
-// export const Api = "https://mishcofinal.onrender.com";   
+export const Api = "https://mishcofinal.onrender.com";   
 // src/api.js
-export const Api = "http://localhost:5000";   // ✅ Correct: Using HTTP on local development port
+// export const Api = "http://localhost:5000";   
